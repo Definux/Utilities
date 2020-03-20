93018e17660472241b8a67ea7c4e11ac1a0e0362
gsk567 <georgi.karagogov@gmail.com>
2020-03-20 03:03:58 +0200

    Add object extension

---
+++ src/Definux.Utilities.Extensions/ObjectExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Definux.Utilities.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static Guid GetGuidValueOrDefault(this object value)
+        {
+            Guid resultGuid = Guid.Empty;
+            if (value != null)
+            {
+                Guid.TryParse(value.ToString(), out resultGuid);
+            }
+
+            return resultGuid;
+        }
+    }
+}

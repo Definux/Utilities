4c975436ecf3aa6d72536e787b3acb2bf2217c01
gsk567 <georgi.karagogov@gmail.com>
2020-04-27 20:25:09 +0300

    update created result

---
+++ src/Definux.Utilities.Objects/CreatedResult.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Definux.Utilities.Objects
+{
+    public class CreatedResult
+    {
+        private int? createdEntityAsInt;
+        private Guid? createdEntityIdAsGuid;
+
+        public CreatedResult(int createdEntityId)
+        {
+            this.createdEntityAsInt = createdEntityId;
+        }
+
+        public CreatedResult(Guid createdEntityId)
+        {
+            this.createdEntityIdAsGuid = createdEntityId;
+        }
+
+        public string CreatedEntityId 
+        { 
+            get
+            {
+                if (this.createdEntityAsInt.HasValue)
+                {
+                    return this.createdEntityAsInt.Value.ToString();
+                }
+                else if (this.createdEntityIdAsGuid.HasValue)
+                {
+                    return this.createdEntityIdAsGuid.Value.ToString();
+                }
+
+                return null;
+            }
+        }
+    }
+}

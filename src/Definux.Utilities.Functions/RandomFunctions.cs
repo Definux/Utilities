deb41b3a19c06b51949ecab0953c02f3fd91d856
gsk567 <georgi.karagogov@gmail.com>
2020-09-02 22:42:19 +0300

    add random functions

---
+++ src/Definux.Utilities.Functions/RandomFunctions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Definux.Utilities.Functions
+{
+    public static class RandomFunctions
+    {
+        private static Random Random = new Random();
+
+        public static T GetRandomElementFromArray<T>(T[] array)
+        {
+            return array[Random.Next(0, array.Length)];
+        }
+
+        public static string GetRandomFileFromDirectory(string directory)
+        {
+            if (Directory.Exists(directory))
+            {
+                var files = Directory.GetFiles(directory);
+                if (files != null && files.Length > 0)
+                {
+                    return GetRandomElementFromArray(files);
+                }
+            }
+
+            return null;
+        }
+    }
+}
